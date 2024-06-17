50bdb627016e109329ff3925618d82893f491269
Shrey Shahi <shrey.shahi@gmail.com>
2024-06-17 14:57:51 -0700

    some cleanup

--- react-tic-tac-toe/src/App.js
+++ react-tic-tac-toe/src/App.js
@@ -74,20 +74,18 @@ function Board({xIsNext, squares, onPlay}) {
 
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [xIsNext, setXIsNext] = useState(true);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
-    setXIsNext(!xIsNext);
     setCurrentMove(nextHistory.length - 1);
   }
 
   function jumpTo(move) {
       setCurrentMove(move);
-      setXIsNext(move % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
