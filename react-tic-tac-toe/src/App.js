01e244e6990bb1e9136a7c839fcf93311a195924
Shrey Shahi <shrey.shahi@gmail.com>
2024-06-17 14:12:58 -0700

    working game yay

--- react-tic-tac-toe/src/App.js
+++ react-tic-tac-toe/src/App.js
@@ -1,20 +1,61 @@
 import {useState} from 'react';
 
+// tutorial: https://react.dev/learn/tutorial-tic-tac-toe
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  
+  for(let i = 0; i < lines.length; i++){
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+    }
+  }
+  return null;
+}
+
 function Square({value, setValue}) {
   return <button className="square" onClick={setValue}>{value}</button>;
 }
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
 
   function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+        return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if(xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
-  
+
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} setValue={() => handleClick(0)}/>
         <Square value={squares[1]} setValue={() => handleClick(1)}/>
